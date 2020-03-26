1482787dcd73a31a14bdaa8698b63fa3cd17e180
Cristea Muller Haris <62335069+Harismaticul@users.noreply.github.com>
2020-03-26 11:30:48 +0200

    Update Conturi_bancare.cpp

--- Conturi_bancare.cpp
+++ Conturi_bancare.cpp
@@ -6,118 +6,133 @@
 #define strcpy_s
 using namespace std;
 
-Accounts::Accounts()
+
+//constructor initial
+Bank::Bank()
 {
-	this->day = 0;
-	this->sum = 0;
-	this->type = NULL;
-	this->description = NULL;
+    this->day = 0;
+    this->money = 0;
+    this->type = NULL;
+    this->desc = NULL;
 }
 
-Accounts::Accounts(int day, int sum, char* type, char* description)
+
+//constructor implicit
+Bank::Bank(int day, int money, char* type, char* desc)
 {
-	this->day = day;
-	this->sum = sum;
-	this->type = new char[strlen(type) + 1];
-	strcpy_s(this->type, strlen(type) + 1, type);
-	this->description = new char[strlen(description) + 1];
-	strcpy_s(this->description, strlen(description) + 1, description);
+    this->day = day;
+
+    this->money = money;
+    
+    this->type = new char[strlen(type) + 1];
+    strcpy_s(this->type, 1 + strlen(type), type);
+    
+    this->desc = new char[strlen(desc) + 1];
+    strcpy_s(this->desc, 1 + strlen(desc), desc);
 }
 
-Accounts::Accounts(const Accounts& a)
+//constructor copiere a unui obiect de tip banca
+Bank::Bank(const Bank& b)
 {
-	this->day = a.day;
-	this->sum = a.sum;
-	this->type = new char[strlen(a.type) + 1];
-	strcpy_s(this->type, strlen(a.type) + 1, a.type);
-	this->description = new char[strlen(a.description) + 1];
-	strcpy_s(this->description, strlen(a.description) + 1, a.description);
+    this->day = b.day;
+    
+    this->money = b.money;
+
+    this->type = new char[strlen(b.type) + 1];
+    strcpy_s(this->type, 1 + strlen(b.type), b.type);
+
+    this->desc = new char[strlen(b.desc) + 1];
+    strcpy_s(this->desc, 1 + strlen(b.desc), b.desc);
+    
+
 }
 
-Accounts::~Accounts()
+// destructor pentru un obiect bank
+Bank::~Bank()
 {
-	this->day = 0;
-	this->sum = 0;
-	if (this->type != NULL)
-	{
-		delete[] this->type;
-		this->type = NULL;
-	}
-	if (this->description != NULL)
-	{
-		delete[] this->description;
-		this->description = NULL;
-	}
+    if (this->type)
+    {
+        delete[] this->type;
+        this->type = NULL;
+    }
+
+    if (this->desc)
+    {
+        delete[] this->desc;
+        this->desc = NULL;
+    }
 }
 
-int Accounts::getDay()
-{
-	return this->day;
+// cheltuiala cheltuiala
+int Bank::getDay() {
+    return this->day;
 }
 
-int Accounts::getSum()
-{
-	return this->sum;
+//getter suma de bani
+int Bank::getMoney() {
+    return this->money;
 }
 
-char* Accounts::get_type()
-{
-	return this->type;
+//getter tip cheltuiala
+char* Bank::getType() {
+    return this->type;
+}
+//getter descriere cheltuiala
+char* Bank::getDesc() {
+    return this->desc;
 }
 
-char* Accounts::get_description()
+//setter zi cheltuiala
+void Bank::setDay(int day)
 {
-	return this->description;
+    this->day = day;
 }
 
-void Accounts::set_day(int day)
+//setter suma de bani
+void Bank::setMoney(int money)
 {
-	this->day = day;
+    this->money = money;
 }
 
-void Accounts::set_sum(int sum)
+//setter tip cheltuiala
+void Bank::setType(char* type)
 {
-	this->sum = sum;
+    if (this->type)
+    {
+        delete[] this->type;
+    }
+    this->type = new char[strlen(type) + 1];
+    strcpy_s(this->type, strlen(type) + 1, type);
+}
+ 
+//setter descriere cheltuiala
+void Bank::setDesc(char* desc)
+{
+    if (this->desc)
+    {
+        delete[] this->desc;
+    }
+    this->desc = new char[strlen(desc) + 1];
+    strcpy_s(this->desc, strlen(desc) + 1, desc);
 }
 
-void Accounts::set_type(char* type)
-{
-	if (this->type != NULL)
-	{
-		delete[] this->type;
-		this->type = NULL;
-	}
-	this->type = new char[strlen(type) + 1];
-	strcpy_s(this->type, strlen(type) + 1, type);
+//operator de copiere valoare
+Bank& Bank::operator=(const Bank& b) {
+    this->setDay(b.day);
+    this->setMoney(b.money);
+    this->setDesc(b.desc);
+    this->setType(b.type);
+    return *this;
 }
 
-void Accounts::set_description(char* description)
-{
-	if (this->description != NULL)
-	{
-		delete[] this->description;
-		this->description = NULL;
-	}
-	this->description = new char[strlen(description) + 1];
-	strcpy_s(this->description, strlen(description) + 1, description);
+// operator de comparare
+bool Bank:: operator==(const Bank& b) {
+    return strcmp(this->type, b.type) == 0 && strcmp(this->desc, b.desc) == 0 &&  this->day == b.day && this->money == b.money;
 }
 
-Accounts& Accounts::operator=(const Accounts& a)
+//declararea operatie de afisare clasa
+ostream& operator<<(ostream& os, const Bank& b)
 {
-	this->set_day(a.day);
-	this->set_sum(a.sum);
-	this->set_type(a.type);
-	this->set_description(a.description);
-	return *this;
-}
-
-bool Accounts::operator==(const Accounts& a)
-{
-	return (this->day == a.day) && (this->sum == a.sum) && (strcmp(this->type, a.type) == 0) && (strcmp(this->description, a.description) == 0);
-}
-
-ostream& operator<<(ostream& os, const Accounts& a)
-{
-	os << "Day: " << a.day << "\nSum: " << a.sum << "\nType: " << a.type << "\nDescription: " << a.description;
-	return os;
+    os << b.day << " " << b.money << " " << b.type << " " << b.desc;
+    return os;
 }
